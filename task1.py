497918e7f0b36023ddd1a0a80572fbfe556050de
barseghyanaram793-design <barseghyanaram793@gmail.com>
2025-10-29 09:13:44 +0100

    added task1

---
+++ task1.py
@@ -0,0 +1,99 @@
+
+
+age=44
+print(age)
+
+year=1981
+month=6
+day=27
+print(year," ", month," ", day)
+
+
+a=10
+b=3,5
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+
+num = '25'
+print(type(num))
+print(int(num))
+print(type(int(num)))
+
+price = "19.99"
+print(type(price))
+print(float(price))
+print(type(float(price)))
+
+
+
+temperature = 36.6
+print(temperature)
+print(int(temperature))
+
+
+word = "Python"
+print(word)
+
+name = "Aram"
+surname = "Barseghyan"
+city = "Yerevan"
+print(name," ",surname," ",city)
+
+
+text = "Learning is fun"
+print(text*2)
+
+text = "Hello"
+print(text[0])
+print(text[-1])
+
+
+text = "Hello"
+print(len(text))
+print(text)
+
+
+text = "Programming"
+print(text[3])
+print(text[5])
+print(text[8])
+
+text = "Armenia"
+print(text[:3])
+
+text = "Armenia"
+print(text[:-2])
+
+text = "I love Python"
+text2 = text.replace("Python","Coding")
+print(text2)
+
+text = "hello"
+print(text.upper())
+
+text = "good morning"
+print(text.upper())
+
+text = "PYTHON IS FUN"
+print(text.lower())
+
+
+text = "hello world"
+print(text.capitalize())
+
+text = "I love apples"
+text2 = text.replace("apples","bananas")
+print(text2)
+
+text = "Hello world from Python"
+print(text.split(" "))
+
+
+
+
+
+
+
