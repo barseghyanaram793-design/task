75d7a413aa2a0ea7968a53140d99d47e97ee50bf
barseghyanaram793-design <barseghyanaram793@gmail.com>
2025-11-10 12:14:13 +0100

    initial commit

--- task1.py
+++ task1.py
@@ -91,9 +91,505 @@ print(text2)
 text = "Hello world from Python"
 print(text.split(" "))
 
+"""print("xndir1")
+number1 = int(input("enter number: "))
+print((number1**2)*2)"""
+
+"""print("xndir2")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a+b+c)/2)"""
+
+"""print("xndir3")
+a = int(input("enter number: "))
+print(5*a-a*2)"""
+
+"""print("xndir4")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a+b)*(b+c))"""
+
+"""print("xndir5")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a+b)**2)"""
+
+"""print("xndir6")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print(a**2+b**2+2*a*b+c)"""
 
 
+"""print("xndir7")
+a = int(input("enter number: "))
+print((a**2)/2)"""
+
+"""print("xndir8")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a-b)**2 / (a-b))"""
+
+"""print("xndir9")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+d = int(input("enter number: "))
+print((a+b)*(c-d))"""
+
+"""print("xndir10")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a**2-b**2)/2)"""
+
+"""print("xndir11")
+a = int(input("enter number: "))
+print(a*2)"""
+
+"""print("xndir12")
+a = int(input("enter number: "))
+print((a+10)/2)"""
+
+"""print("xndir13")
+a = int(input("enter number: "))
+print(a**3-a**2)"""
+
+"""print("xndir14")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a+b)*c)"""
+
+"""print("xndir15")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a%b)"""
+
+"""print("xndir16")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a//b)"""
+
+"""print("xndir17")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a*b+c)/(b+1))"""
+
+"""print("xndir18")
+a = int(input("enter number: "))
+print(a+a**2)"""
+
+"""print("xndir19")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a+b)**3)"""
+
+"""print("xndir20")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print(((a+b)**2+c)/(a+1))"""
+#Operatorner
+"""print("xndir1")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a+b)"""
+
+"""print("xndir2")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a-b)"""
+
+"""print("xndir3")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a*b)"""
+
+"""print("xndir4")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a//b)
+print(a%b)"""
+
+"""print("xndir5")
+a = int(input("enter number: "))
+print(a**2)
+print(a**3)"""
+
+"""print("xndir6")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a+b)*c)"""
+
+"""print("xndir7")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a+b+c)-(a**2*b))"""
+
+"""print("xndir8")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+d = int(input("enter number: "))
+e = int(input("enter number: "))
+print((a+b)*(c-d)/e)"""
+
+"""print("xndir9")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a+b)**3-b)"""
+
+"""print("xndir10")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+d = int(input("enter number: "))
+print((a**2+b**2)/(c+d))"""
+
+"""print("xndir11")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+d = int(input("enter number: "))
+e = int(input("enter number: "))
+print((a*b+c)//(d-e))"""
+
+#Veragrman operatorner
+
+"""print("xndir1")
+a = int(input("enter number: "))
+a += 5
+print(a)"""
+
+"""print("xndir2")
+a = int(input("enter number: "))
+a*=10
+print(a)"""
+
+"""print("xndir3")
+a = int(input("enter number: "))
+a /= 2
+print(a)"""
+
+"""print("xndir4")
+a = int(input("enter number: "))
+a **= 3
+print(a)"""
+
+"""print("xndir5")
+a = int(input("enter number: "))
+a += 5
+a *= 3
+a /= 2
+print(a)"""
+
+"""print("xndir6")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a**2)," ",(b+a)," ",(c-b))"""
+
+"""print("xndir7")
+a = int(input("enter number: "))
+print(((a+10)/4)**2)"""
+
+#Hamematman operatorner
+"""print("xndir1")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a<b)"""
+
+"""print("xndir2")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a>=b)"""
+
+"""print("xndir3")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a==b)"""
+
+"""print("xndir4")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a!=b)"""
+
+"""print("xndir5")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a+b)>c) and ((b-c)!=a)"""
+
+"""print("xndir6")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print((a**2>=b)or(c<a*b))"""
+
+"""print("xndir7")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a>=b) and a<b**2))"""
+
+#Nuynutyan operatorner
+"""print("xndir1")
+text1 = input("enter text: ")
+text2 = input("enter text: ")
+print(text1 is text2)"""
 
 
+"""print("xndir2")
+text1 = input("enter text: ")
+text2 = input("enter text: ")
+print(text1 is not text2)"""
+
+"""print("xndir3")
+text1 = input("enter text: ")
+text2 = input("enter text: ")
+print(text1 is text2)"""
+
+"""print("xndir4")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a is not b)
+print(a!=b)"""
+
+#Patkaneliutyan op.
+"""print("xndir1")
+x = input("enter text: ")
+print("r" in x)"""
+
+"""print("xndir2")
+x = input("enter text: ")
+print("h" not in x)"""
+
+"""print("xndir3")
+x = input("enter text: ")
+print("k" in x and "z" not in x)"""
+
+#xndir4?
+
+
+#Tramabanakan operatorner
+"""print("xndir1")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a>0 and b>0)"""
+
+"""print("xndir2")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print(a<0 or b<0)"""
+
+"""print("xndir3")
+a = int(input("enter number: "))
+print(not a<0)"""
+
+"""print("xndir4")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print(a>b and (b<c) or not c<a )"""
+
+"""print("xndir5")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+print((a>0 and b>0 and (a+b)%2==0))"""
+
+"""print("xndir6")
+a = int(input("enter number: "))
+b = int(input("enter number: "))
+c = int(input("enter number: "))
+print(a<b) or (b>c) and not (a==c)"""
+
+#Arajadranq 3
+"""print("xndir1")
+a = int(input("Enter number: "))
+if a>0:
+    print("Drakan")
+else:
+    ("Bacasakan kam 0")"""
+
+"""print("xndir2")
+a = int(input("Enter number: "))
+if a % 2 == 0:
+    print("Zuyg tiv")
+else:
+    print("Kent")"""
+#?
+
+"""print("xndir3")
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+if a > b:
+    print("a tiv met e")
+if b>a:
+    print("b tiv met e")
+else:
+    print("a=b")"""
+
+"""print("xndir4")
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+c = int(input("Enter number: "))
+if a >= b and a >= c:
+    print("a tiv met e")
+elif b >= a and b >= c:
+    print("b tiv met e")
+elif c >= a and c >= b:
+    print("c tiv met e")
+else:
+    print("bolor tvere drakan en")"""
+
+"""print("xndir5")
+a = int(input("Enter number:"))
+if a > 0:
+    print("a tiv Drakan e")
+elif a < 0:
+    print("a tiv Bacasakan e")
+else:
+    print("a = 0")"""
+
+"""print("xndir6")
+gnahatakan = int(input("Enter gnahatakan: "))
+if 90 <= gnahatakan <= 100:
+    print("Excellent")
+elif 70 <= gnahatakan <= 89:
+    print("Good")
+elif 50 <= gnahatakan <= 69:
+    print("Average")
+elif 0 <= gnahatakan <= 49:
+    print("Fail")
+else:
+    print("invalid input")"""
+
+"""print("xndir7")
+gin = int(input("Enter gin"))
+if 10000 <= gin <= 50000:
+    print("Zexch 10%")
+elif gin >= 50000:
+    print("Zexch 30%")
+elif gin < 0:
+    print("Invalid input")
+else:
+    print("Zexch 0%")"""
+
+"""print("xndir8")
+age = int(input("Enter tariq"))
+passport = input("Enter yes or no")
+if age <= 0:
+    print("Invalid input")
+elif age >= 18:
+    if passport == "yes":
+        print("Karox e qvearkel")
+    elif age < 18:
+        if passport == "no":
+            print("Chi karox qvearkel")
+else:
+    print("Invalid input")"""
+
+"""print("xndir9")
+gnahatakan = int(input("Enter gnahatakan: "))
+if  0 > gnahatakan > 100:
+    print("Invalid input")
+ararka = input("Enter math or english")
+
+if gnahatakan >= 50:
+    if ararka == "math":
+        print("Ancel e math")
+    elif ararka == "english":
+        print("Ancel e english")
+    else:
+        print("Chi ancel")"""
+#?
+
+"""print("xndir10")
+a = int(input("Enter number: "))
+print("Drakan") if a > 0  else  print("bacasakan")"""
+
+"""print("xndir11")
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+print("Met e a ") if a > b  else  print ("Met e b")"""
+
+"""print("xndir12")
+gin = int(input("Enter gin: "))
+if gin <= 0:
+    print("Invalid input")
+elif gin >= 10000:
+    print("Zexch ka")
+else:
+    print("Zexch chka")"""
+
+"""print("xndir13")
+a = int(input("Enter number: "))
+b = int(input("Enter number: "))
+c = int(input("Enter number: "))
+if a < 0:
+    print("Invalid input")
+elif a > b:
+    if a > c:
+        print("a amenametn e")
+    else:
+        print("a < c")
+else:
+    print("a poqr kam havasar e b")"""
+
+"""print("xndir14")
+t = int(input("Enter t°: "))
+if t < 0:
+    print("Sarn e")
+elif t == 0:
+    print("Haeli e")
+else:
+    print("Taq e")"""
+
+"""print("xndir15")
+month = int(input("Enter month: "))
+if 0 >= month > 12:
+    print("Invalid input")"""
+#?
+
+"""print("xndir16")
+a = int(input("Enter number: "))
+a = a * 4 if a > 0  else  print("a")"""
+
+"""print("xndir17")
+account = input("Enter account(savings or current ): ")
+amount = int(input("Enter amount: "))
+if account == "savings":
+    if amount > 100000:
+       print("High balance")
+    else:
+        print("Normal balanse")
+elif account == "current":
+    print("Business balanse")
+else:
+    print("Anhayt balanse")"""
+
+"""print("xndir18")
+exam = int(input("Enter miavore: "))
+assignment = int(input("Enter miavore: "))
+if exam >= 50 and assignment >= 50:
+    print("Ancel e")
+elif exam >= 50 or assignment >= 50:
+    print("paymanakan e ancel")
+else:
+    print("Chi ancel")"""
+
+"""print("xndir19")
+a = int(input("Enter amboxj tiv: "))
+if a > 0:
+    if a % 2 == 0:
+        print("Drakan ev zuyg")
+    else:
+        print("Drakan, bayc kent e")
+else:
+    print("Drakan che")"""
 
 
